7acf48fcaf05a1e20847152c45d876eacd404fc9
donwheeler <slowheelerfam@gmail.com>
2026-01-08 13:37:43 -0800

    Finished Highlights25

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 //import './App.css';
+import Highlights from './components/Highlights';
+import Landing from './components/Landing';
 import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
         <Nav />
+        <Landing />
+        <Highlights />
     </div>
   );
 }
