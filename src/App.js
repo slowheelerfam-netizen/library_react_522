0e25376598371e4760395c27c5f948b85094f9d5
donwheeler <slowheelerfam@gmail.com>
2026-01-08 17:00:19 -0800

    Finished Featured Books section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Highlights from './components/Highlights';
 import Landing from './components/Landing';
 import Nav from './components/Nav';
+import Featured from './components/Featured';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Nav />
         <Landing />
         <Highlights />
+        <Featured />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags  } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt  } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags )
+library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode> 
 );
 
 // If you want to start measuring performance in your app, pass a function
